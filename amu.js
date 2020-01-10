9490ad2e69f9cfaf8655920606826e67098d3244
siloFOX <ladnav0704@gmail.com>
2020-01-10 21:51:26 +0900

    fix : Remove comment in amu.js

--- amu.js
+++ amu.js
@@ -5,15 +5,6 @@ app.get('/', function (req, res) {
     res.send("hello world!")
 })
 
-// {
-//     const app = 10
-//     console.log(app)
-// }
-
-// app.get('/', (req, res) => {
-//     res.send("hello world!")
-// })
-
 app.get('/min/baboo', (req, res) => {
     res.send("드디어 해냈다!")
 })
